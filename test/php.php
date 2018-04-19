28c31af9d25de6d943d4c10b3ef26382fe6e331f
AdelTaiebTamacha <35687495+AdelTaiebTamacha@users.noreply.github.com>
2018-04-19 10:04:07 +0200

    test

---
+++ test/php.php
@@ -0,0 +1,9 @@
+<?php
+			function show($string) {
+				
+				echo "<p>".$string."</p>";
+			}		
+			show($_POST["data"])	
+				
+?>
+
---
+++ test/test.php
@@ -0,0 +1,27 @@
+<html>
+	<head>
+	  	
+		
+	</head>
+
+	<body>
+	
+		
+			
+		<?php
+			function show($string) {
+				
+				print "<p>".$string."</p>";
+			}		
+					
+				
+		?>
+			
+		<form method="post" action= "php.php">  
+		Name: <input type="text" name="data" value="">
+		<input type="submit" name="submit" value="Submit">  
+		</form>
+		
+	</body>
+
+</html>
